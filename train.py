6b761324012ab80526f188b2d83bd034f69af5d8
Pranav Bansal <gunubansal129@gmail.com>
2024-07-25 00:16:07 +0530

    remove train.py

--- train.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-
-    model = YOLO('yolov8l.yaml')  # Load model
-
-    results = model.train(data='D:/C_Drive/Pictures/IIITD Dataset/data/config.yaml', epochs = 20)  # Train model
-    # model.export('trained_model')  # Save model
